44ba98f835f3fad69b3784c348632c7b2b7d68dc
P-MBD <112797204+P-MBD@users.noreply.github.com>
2023-02-18 19:08:16 +0330

    register user functionalities and the home page setup

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,11 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
+#media file configuration
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
